2165c8113af88c3eafdc978bb3490aaab5bde69c
Pierre-Yves Flamand <46058408+PierreYvesFlamand@users.noreply.github.com>
2022-01-17 15:01:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,50 +37,6 @@ class userScript {
     }
 }
 
-const USERSCRIPTS = [
-    {
-        order: 0,
-        name: "Melvor Items Drop Rates",
-        description: "Shows monsters, chests and already acquired thieving npcs items drop rates",
-        scriptID: "435520",
-        version: "v1.0.1",
-        imageGalleryName: [
-            "monsterItemsDropRatesExample",
-            "chestItemsDropRatesExample",
-            "thievingNpcItemsDropRatesExample",
-        ],
-        todos: ["Show thieving item and drop rate when not already acquired"],
-    },
-    {
-        order: 1,
-        name: "Melvor Better Completion Log",
-        description:
-            "Shows images of undiscovered items, monsters, pets and open wiki page on click",
-        scriptID: "438058",
-        version: "v1.0.1",
-        imageGalleryName: ["betterCompletionLogExample", "betterCompletionLogTooltipExample"],
-        todos: ["Adding non completion log entries in a separate tab"],
-    },
-    {
-        order: 2,
-        name: "Melvor Auto Loot",
-        description: "Add an Auto Loot toggle in combat",
-        scriptID: "435548",
-        version: "v1.0.1",
-        imageGalleryName: ["autoLootExample"],
-    },
-    {
-        order: 3,
-        name: "Melvor One Click Farming",
-        description:
-            "Harvest All, Apply Weird Gloop to all Plots and Plant All Selected Crops on every farming areas in one click",
-        scriptID: "438680",
-        version: "v1.0.1",
-        imageGalleryName: ["oneClickFarmingExample"],
-        todos: ["Adding a customization menu to choose actions to perform"],
-    },
-];
-
 USERSCRIPTS.forEach((USERSCRIPTData) => {
     // Fetch data
     fetch(`https://greasyfork.org/en/scripts/${USERSCRIPTData.scriptID}/stats.json`)
